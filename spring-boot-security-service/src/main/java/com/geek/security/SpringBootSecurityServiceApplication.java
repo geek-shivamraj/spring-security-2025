e624c01d54597938d6ed0879e158f753e2f50f97
Shivam-Omen <srvcode@gmail.com>
2025-06-28 18:58:50 +0530

    Adding Spring Security Base module

---
+++ spring-boot-security-service/src/main/java/com/geek/security/SpringBootSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.geek.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityServiceApplication.class, args);
+	}
+
+}
