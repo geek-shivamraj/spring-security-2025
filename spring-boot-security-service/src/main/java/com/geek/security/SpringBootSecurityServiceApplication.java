283a12a965c41fb82205e187543d7efd592bdc76
Shivam-Omen <srvcode@gmail.com>
2025-06-29 10:53:46 +0530

    Adding mvc config for thymeleaf template

--- spring-boot-security-service/src/main/java/com/geek/security/SpringBootSecurityServiceApplication.java
+++ spring-boot-security-service/src/main/java/com/geek/security/SpringBootSecurityServiceApplication.java
@@ -3,6 +3,10 @@ package com.geek.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *  If Spring Security is on the classpath,
+ *  Spring Boot automatically secures all HTTP endpoints with “basic” authentication. However, you can further customize the security settings.
+ */
 @SpringBootApplication
 public class SpringBootSecurityServiceApplication {
 
---
+++ spring-boot-security-service/src/main/java/com/geek/security/config/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.geek.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * The web application is based on Spring MVC. As a result, we need to configure Spring MVC and set up view controllers to expose these templates.
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
