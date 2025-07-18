aa4b7446c442181617f7fc6bc36b88453cf0cb9b
Shivam-Omen <srvcode@gmail.com>
2025-07-18 13:48:32 +0530

    Adding section 7 base

---
+++ spring-security-course/springsection7/src/main/java/com/geek/spring/security/config/ProjectSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.geek.spring.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * we use the @Profile annotation to specify that this component/bean should only be activated in the non-prod profile.
+ *
+ * In lower level environment, we want to permit all the requests.
+ */
+@Configuration
+@Profile("!prod")
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrfConfigurer -> csrfConfigurer.disable())
+                .authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    /**
+     * We can refer PasswordEncoderFactories.createDelegatingPasswordEncoder() to know the list of supported encoders
+     * along with the default encoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     * CompromisedPasswordChecker is used to check if the password is compromised
+     * This bean will enforce the user to use a strong password
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
+
