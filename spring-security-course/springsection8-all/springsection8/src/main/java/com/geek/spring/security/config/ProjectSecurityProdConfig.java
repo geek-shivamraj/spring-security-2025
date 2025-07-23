64c21240c396bad0d7a2c903fa4741968e44dee6
Shivam-Omen <srvcode@gmail.com>
2025-07-23 21:01:41 +0530

    Adding CORS configuration

--- spring-security-course/springsection8-all/springsection8/src/main/java/com/geek/spring/security/config/ProjectSecurityProdConfig.java
+++ spring-security-course/springsection8-all/springsection8/src/main/java/com/geek/spring/security/config/ProjectSecurityProdConfig.java
@@ -2,6 +2,7 @@ package com.geek.spring.security.config;
 
 import com.geek.spring.security.exceptionhandling.CustomAccessDeniedHandler;
 import com.geek.spring.security.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,6 +14,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -22,7 +27,20 @@ public class ProjectSecurityProdConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession").maximumSessions(1))
+        http.cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession").maximumSessions(1))
                 .redirectToHttps(https -> https.requestMatchers(AnyRequestMatcher.INSTANCE))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
@@ -44,4 +62,3 @@ public class ProjectSecurityProdConfig {
         return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
-
