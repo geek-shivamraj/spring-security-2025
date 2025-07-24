248d2ca27458d097b4e101ad487203b2a23bf953
Shivam-Omen <srvcode@gmail.com>
2025-07-24 08:16:24 +0530

    Adding Section 8 base

---
+++ spring-security-course/springsection9/src/main/java/com/geek/spring/security/model/Customer.java
@@ -0,0 +1,39 @@
+package com.geek.spring.security.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Entity
+@Getter @Setter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    /**
+     * JsonProperty.Access.WRITE_ONLY is used to hide the password while returning the response
+     */
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String pwd;
+
+    private String role;
+
+    @Column(name = "create_dt")
+    @JsonIgnore
+    private Date createDt;
+
+}
---
+++ spring-security-course/springsection9/src/main/java/com/geek/spring/security/repository/AccountTransactionsRepository.java
@@ -0,0 +1,14 @@
+package com.geek.spring.security.repository;
+
+import com.geek.spring.security.model.AccountTransactions;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AccountTransactionsRepository extends CrudRepository<AccountTransactions, String> {
+	
+	List<AccountTransactions> findByCustomerIdOrderByTransactionDtDesc(long customerId);
+
+}
