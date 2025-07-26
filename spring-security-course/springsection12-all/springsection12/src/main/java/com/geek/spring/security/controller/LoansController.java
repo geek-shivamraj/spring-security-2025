57e97b55f85a80bc639bd3ffeb366dea43ed5478
Shivam-Omen <srvcode@gmail.com>
2025-07-26 08:38:24 +0530

    Adding method level security using @PreAuthorize, @PostAuthorize, @PreFilter, @PostFiler

--- spring-security-course/springsection12-all/springsection12/src/main/java/com/geek/spring/security/controller/LoansController.java
+++ spring-security-course/springsection12-all/springsection12/src/main/java/com/geek/spring/security/controller/LoansController.java
@@ -3,6 +3,7 @@ package com.geek.spring.security.controller;
 import com.geek.spring.security.model.Loans;
 import com.geek.spring.security.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,8 @@ public class LoansController {
     private final LoanRepository loanRepository;
 
     @GetMapping("/myLoans")
+    @PostAuthorize("hasRole('USER')")
+    //@PostAuthorize("hasRole('ROOT')") // Negative Case
     public List<Loans> getLoanDetails(@RequestParam long id) {
         List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
         if (loans != null) {
