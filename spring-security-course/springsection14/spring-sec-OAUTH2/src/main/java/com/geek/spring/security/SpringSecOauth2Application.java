904bf9a9a2d63eb33845fc90dca4a1ce49e9fbd1
Shivam-Omen <srvcode@gmail.com>
2025-07-26 20:51:21 +0530

    Adding section 14 base

---
+++ spring-security-course/springsection14/spring-sec-OAUTH2/src/main/java/com/geek/spring/security/SpringSecOauth2Application.java
@@ -0,0 +1,13 @@
+package com.geek.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecOauth2Application.class, args);
+	}
+
+}
