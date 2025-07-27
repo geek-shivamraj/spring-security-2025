da38a0865796ca0802125228bbcb9f7930945a95
Shivam-Omen <srvcode@gmail.com>
2025-07-27 19:48:10 +0530

    Adding Auth Server Base

---
+++ spring-security-course/springsection16-all/auth-server/src/main/java/com/geek/spring/security/AuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.geek.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApplication.class, args);
+	}
+
+}
