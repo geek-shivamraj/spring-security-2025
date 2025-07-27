85068e8b951ef27eb7ed48b541c9c493d93930f3
Shivam-Omen <srvcode@gmail.com>
2025-07-27 13:43:57 +0530

    Refactoring to make this application as Resource Server
    1. [Deleting all Custom Authenticator Providers & UserDetails Service] This application will no more perform authentication & Keycloak Auth server will be responsible for the same.
    
    2.Removing AuthenticationManager, CompromisedPasswordChecker, PasswordEncoder Bean.
    
    3. Removing all the Custom filters except CsrfFilter
    
    4. Removing /apiLogin API that generates JWT token from UserController
    
    5. Removing /register API as we don't want our Resource server to support end user registration process.

--- spring-security-course/springsection15-all/springsection15/src/main/java/com/geek/spring/security/config/ProjectSecurityConfig.java
+++ spring-security-course/springsection15-all/springsection15/src/main/java/com/geek/spring/security/config/ProjectSecurityConfig.java
@@ -6,16 +6,9 @@ import com.geek.spring.security.filter.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
@@ -48,11 +41,6 @@ public class ProjectSecurityConfig {
                         .csrfTokenRequestHandler(csrfTokenRequestAttribute)
                         .ignoringRequestMatchers("/contact", "/register", "/apiLogin"))
                 .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
-                .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
-                .addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
-                .addFilterBefore(new JWTTokenValidationFilter(), BasicAuthenticationFilter.class)
                 .cors(corsConfig -> corsConfig.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
@@ -78,34 +66,4 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    /**
-     * We can refer PasswordEncoderFactories.createDelegatingPasswordEncoder() to know the list of supported encoders
-     * along with the default encoder
-     */
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    /**
-     * CompromisedPasswordChecker is used to check if the password is compromised
-     * This bean will enforce the user to use a strong password
-     */
-    @Bean
-    public CompromisedPasswordChecker compromisedPasswordChecker() {
-        return new HaveIBeenPwnedRestApiPasswordChecker();
-    }
-
-    /**
-     * Default value for eraseCredentialsAfterAuthentication is true
-     * providerManager.setEraseCredentialsAfterAuthentication(false) means provider manager will not erase the password inside the Authentication object.
-     */
-    @Bean
-    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-        EazyBankUsernamePwdAuthenticationProvider authenticationProvider =
-                new EazyBankUsernamePwdAuthenticationProvider(userDetailsService, passwordEncoder);
-        ProviderManager providerManager = new ProviderManager(authenticationProvider);
-        providerManager.setEraseCredentialsAfterAuthentication(false);
-        return  providerManager;
-    }
 }
--- spring-security-course/springsection15-all/springsection15/src/main/java/com/geek/spring/security/config/ProjectSecurityProdConfig.java
+++ spring-security-course/springsection15-all/springsection15/src/main/java/com/geek/spring/security/config/ProjectSecurityProdConfig.java
@@ -6,17 +6,10 @@ import com.geek.spring.security.filter.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
@@ -42,11 +35,6 @@ public class ProjectSecurityProdConfig {
                         .csrfTokenRequestHandler(csrfTokenRequestAttribute)
                         .ignoringRequestMatchers("/contact", "/register", "/apiLogin"))
                 .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
-                .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
-                .addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
-                .addFilterBefore(new JWTTokenValidationFilter(), BasicAuthenticationFilter.class)
                 .cors(corsConfig -> corsConfig.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(Collections.singletonList("https://localhost:4200"));
@@ -72,22 +60,4 @@ public class ProjectSecurityProdConfig {
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    public CompromisedPasswordChecker compromisedPasswordChecker() {
-        return new HaveIBeenPwnedRestApiPasswordChecker();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-        EazyBankUsernamePwdAuthenticationProvider authenticationProvider =
-                new EazyBankUsernamePwdAuthenticationProvider(userDetailsService, passwordEncoder);
-        ProviderManager providerManager = new ProviderManager(authenticationProvider);
-        providerManager.setEraseCredentialsAfterAuthentication(false);
-        return  providerManager;
-    }
 }
